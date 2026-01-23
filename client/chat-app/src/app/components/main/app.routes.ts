04d52b25e110cd2c922616652980949526754273
Tysaic <mendezlinaresi@gmail.com>
2026-01-23 15:53:46 -0300

    Components move to their correct folder.

--- client/chat-app/src/app/components/main/app.routes.ts
+++ client/chat-app/src/app/components/main/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { authGuard, guestGuard } from './guards/auth.guard';
+import { authGuard, guestGuard } from '../../guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -9,19 +9,19 @@ export const routes: Routes = [
     },
     {
         path: 'login',
-        loadComponent: () => import('./pages/login/login.component')
+        loadComponent: () => import('../../pages/login/login.component')
         .then(m => m.LoginComponent),
         canActivate: [guestGuard]
     },
     {
         path: 'signup',
-        loadComponent: () => import('./pages/signup/signup.component')
+        loadComponent: () => import('../../pages/signup/signup.component')
         .then(m => m.SignUpComponent),
         canActivate: [guestGuard]
     },
     {
         path: 'chat',
-        loadComponent: () => import('./pages/chat/chat.component')
+        loadComponent: () => import('../../pages/chat/chat.component')
         .then(m => m.ChatComponent),
         canActivate: [authGuard]
     },
