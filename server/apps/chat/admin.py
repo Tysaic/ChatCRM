cce39ac4c3d7f7ecc4ef6c69c7c43e8298d53fd9
Tysaic <mendezlinaresi@gmail.com>
2026-01-26 14:12:33 -0300

    Displaying admin site to chatrooms and chats with search fields

--- server/apps/chat/admin.py
+++ server/apps/chat/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import ChatRoom, ChatMessage
 
 
-admin.site.register(ChatRoom)
-admin.site.register(ChatMessage)
+class ChatRoomAdmin(admin.ModelAdmin):
+
+    list_display = ('roomId', 'name', 'type', 'get_members')
+    list_filter = ('type',)
+    search_fields = (
+        'roomId', 'name', 'member__username', 
+        'member__first_name', 'member__last_name', 'member__email'
+    )
+    filter_horizontal = ('member',)
+    
+    def get_members(self, obj):
+        return ", ".join([member.username for member in obj.member.all()])
+    
+    get_members.short_description = 'Members'
+
+
+class ChatMessageAdmin(admin.ModelAdmin):
+
+    list_display = ('room', 'user', 'message', 'timestamp')
+    list_filter = ('timestamp',)
+    search_fields = ('message',)
+
+admin.site.register(ChatRoom, ChatRoomAdmin)
+admin.site.register(ChatMessage, ChatMessageAdmin)
