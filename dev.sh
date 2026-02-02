598ab80e6e0782fd8a142869b93a4cf2f1e6f0e7
Tysaic <mendezlinaresi@gmail.com>
2026-02-02 14:03:39 -0300

    Developer bash terminal upgrade.

--- dev.sh
+++ dev.sh
@@ -7,13 +7,19 @@ SESSION="V0X";
 
 tmux new-session -d -s $SESSION
 
-tmux send-keys -t $SESSION "cd $PROJECT_ROOT/client/chat-app && npm start" C-m
+# Angular FrontEnd
+tmux send-keys -t $SESSION "cd $PROJECT_ROOT/client/chat-app " C-m
+tmux send-keys -t $SESSION "npm start" C-m
 
+# Backend Django + Daphne
 tmux split-window -h -t $SESSION
-tmux send-keys -t $SESSION "source $ACTIVATE_ENV && cd server && daphne -b 0.0.0.0 -p 8000 V0X.asgi:application" C-m
+tmux send-keys -t $SESSION "source $ACTIVATE_ENV && cd server" C-m
+tmux send-keys -t $SESSION "daphne -b 0.0.0.0 -p 8000 V0X.asgi:application" C-m
 
-
+# Django Admin server
 tmux split-window -v -t $SESSION
-tmux send-keys -t $SESSION "source $ACTIVATE_ENV && cd server && python3 manage.py runserver 0.0.0.0:4000" C-m
+tmux send-keys -t $SESSION "source $ACTIVATE_ENV && cd server" C-m
+tmux send-keys -t $SESSION "python3 manage.py runserver 0.0.0.0:4000" C-m
 
+# Attach to session
 tmux attach-session -t $SESSION
